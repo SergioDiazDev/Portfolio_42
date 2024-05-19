5472353aa40716c238f558cb5d282f09579480a5
Sergio Diaz <SergioDiazDev@gmail.com>
2024-05-20 00:38:36 +0200

    Minishell lista

--- Portfolio.py
+++ Portfolio.py
@@ -1,6 +1,16 @@
 import tkinter as tk
 from tkinter import messagebox
 import subprocess
+import time
+import os
+
+def esperar_archivo(ruta_archivo, tiempo_espera=1, max_intentos=10):
+    intentos = 0
+    while not os.path.exists(ruta_archivo):
+        intentos += 1
+        if intentos >= max_intentos:
+            raise TimeoutError(f"El archivo {ruta_archivo} no se encontró después de {max_intentos} intentos.")
+        time.sleep(tiempo_espera)
 
 def crear_interfaz_con_dos_botones():
     # Crear la ventana principal
@@ -8,13 +18,13 @@ def crear_interfaz_con_dos_botones():
     root.title("Proyectos 42")
     root.geometry("600x400")
 
-
     # Funciones de los botones
     def minishell():
         try:
-            subprocess.run(['gnome-terminal', '--', 'make', '-C', 'minishell/'])
-            subprocess.run(['gnome-terminal', '--', './minishell/minishell'])
+            subprocess.Popen(['gnome-terminal', '--', 'make', '-C', '/root/Desktop/minishell/'])
+            esperar_archivo('/root/Desktop/minishell/minishell')
 
+            subprocess.Popen(['gnome-terminal', '--', '/root/Desktop/minishell/minishell'])
         except Exception as e:
             tk.messagebox.showerror("Error", str(e))
 
